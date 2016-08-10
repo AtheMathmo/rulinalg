a598d6c577affc17c62d61dc570fe0327a4d4b61
Johann Tuffe <tafia973@gmail.com>
2016-08-10 09:48:58 +0800

    Revert "add invertible module with Invertible trait"
    This reverts commit dd1f38c51c6c6da9f164cccfa705c4631610f49c.
    
    Conflicts:
    
    	src/matrix/mod.rs

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -15,7 +15,6 @@ use vector::Vector;
 use self::slice::BaseSlice;
 
 pub mod decomposition;
-pub mod invertible;
 mod impl_ops;
 mod mat_mul;
 mod iter;
@@ -470,8 +469,12 @@ fn parity<T, M>(m: &M) -> T
 
 #[cfg(test)]
 mod tests {
+    use super::super::vector::Vector;
     use super::Matrix;
-    use super::slice::BaseSlice;
+    use super::Axes;
+    use super::slice::{BaseSlice, BaseSliceMut};
+    use super::decomposition::Decomposition;
+    use libnum::abs;
 
     #[test]
     fn test_new_mat() {
@@ -555,6 +558,85 @@ mod tests {
         }
 
         assert_eq!(a[[0, 0]], 13.0);
+    }
+
+    #[test]
+    fn test_matrix_select_rows() {
+        let a = Matrix::new(4, 2, (0..8).collect::<Vec<usize>>());
+
+        let b = a.select_rows(&[0, 2, 3]);
+
+        assert_eq!(b.into_vec(), vec![0, 1, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_matrix_select_cols() {
+        let a = Matrix::new(4, 2, (0..8).collect::<Vec<usize>>());
+
+        let b = a.select_cols(&[1]);
+
+        assert_eq!(b.into_vec(), vec![1, 3, 5, 7]);
+    }
+
+    #[test]
+    fn test_matrix_select() {
+        let a = Matrix::new(4, 2, (0..8).collect::<Vec<usize>>());
+
+        let b = a.select(&[0, 2], &[1]);
+
+        assert_eq!(b.into_vec(), vec![1, 5]);
+    }
+
+    #[test]
+    fn matrix_diag() {
+        let a = Matrix::new(3, 3, vec![1., 3., 5., 2., 4., 7., 1., 1., 0.]);
+
+        let b = a.is_diag();
+
+        assert!(!b);
+
+        let c = Matrix::new(3, 3, vec![1., 0., 0., 0., 2., 0., 0., 0., 3.]);
+        let d = c.is_diag();
+
+        assert!(d);
+    }
+
+    #[test]
+    fn matrix_det() {
+        let a = Matrix::new(2, 2, vec![2., 3., 1., 2.]);
+        let b = a.det();
+
+        assert_eq!(b, 1.);
+
+        let c = Matrix::new(3, 3, vec![1., 2., 3., 4., 5., 6., 7., 8., 9.]);
+        let d = c.det();
+
+        assert_eq!(d, 0.);
+
+        let e = Matrix::<f64>::new(5,
+                                   5,
+                                   vec![1., 2., 3., 4., 5., 3., 0., 4., 5., 6., 2., 1., 2., 3.,
+                                        4., 0., 0., 0., 6., 5., 0., 0., 0., 5., 6.]);
+
+        let f = e.det();
+
+        println!("det is {0}", f);
+        let error = abs(f - 99.);
+        assert!(error < 1e-10);
+    }
+
+    #[test]
+    fn matrix_solve() {
+        let a = Matrix::new(2, 2, vec![2., 3., 1., 2.]);
+
+        let y = Vector::new(vec![8., 5.]);
+
+        let x = a.solve(y).unwrap();
+
+        assert_eq!(x.size(), 2);
+
+        assert_eq!(x[0], 1.);
+        assert_eq!(x[1], 2.);
     }
 
     #[test]
