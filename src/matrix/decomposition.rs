d227259bbfacdd0fc1cd558c0e847c1db0f45ae3
Andreas Longva <andreas.b.longva@gmail.com>
2016-09-22 22:15:51 +0200

    Add SVD benchmarks
    Since the benchmarks test module-internal methods,
    the benchmarks can not reside in the benches/ subdirectory.
    However, since the benchmarks must be run on nightly, it
    involves some complication. Hence, in order to run these
    benchmarks, you must add the 'nightly' feature to the compilation,
    like so:
    
    `cargo bench --features nightly`

--- src/matrix/decomposition.rs
+++ src/matrix/decomposition.rs
@@ -1485,4 +1485,55 @@ mod tests {
 
         let _ = a.lup_decomp();
     }
+
+    #[cfg(all(test, feature = "nightly"))]
+    mod benches {
+        use test::Bencher;
+        use rand;
+        use rand::{Rng, SeedableRng};
+        use matrix::Matrix;
+
+        fn reproducible_random_matrix(rows: usize, cols: usize) -> Matrix<f64> {
+            const STANDARD_SEED: [usize; 4] = [12, 2049, 4000, 33];
+            let mut rng = rand::StdRng::from_seed(&STANDARD_SEED);
+            let elements: Vec<_> = rng.gen_iter::<f64>().take(rows * cols).collect();
+            Matrix::new(rows, cols, elements)
+        }
+
+        #[bench]
+        fn svd_10_10(b: &mut Bencher) {
+            let mat = reproducible_random_matrix(10, 10);
+
+            b.iter(||
+                mat.clone().svd()
+            )
+        }
+
+        #[bench]
+        fn svd_unordered_10_10(b: &mut Bencher) {
+            let mat = reproducible_random_matrix(10, 10);
+
+            b.iter(||
+                mat.clone().svd_unordered()
+            )
+        }
+
+        #[bench]
+        fn svd_100_100(b: &mut Bencher) {
+            let mat = reproducible_random_matrix(100, 100);
+
+            b.iter(||
+                mat.clone().svd()
+            )
+        }
+
+        #[bench]
+        fn svd_unordered_100_100(b: &mut Bencher) {
+            let mat = reproducible_random_matrix(100, 100);
+
+            b.iter(||
+                mat.clone().svd_unordered()
+            )
+        }
+    }
 }
