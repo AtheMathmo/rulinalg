fd911ade448c04267f173ca82613d796b3103917
Andreas Longva <andreas.b.longva@gmail.com>
2016-10-15 19:04:13 +0200

    Make macros module a folder

---
+++ src/macros/mod.rs
@@ -0,0 +1,9 @@
+//! Macros for the linear algebra modules.
+
+#[macro_use]
+mod matrix;
+
+#[macro_use]
+mod matrix_eq;
+
+pub use self::matrix_eq::{elementwise_matrix_comparison, AbsoluteElementwiseComparator};
