daa8526c3437444b0a604f260a3dce4f3a7686e7
Andreas Longva <andreas.b.longva@gmail.com>
2016-10-16 19:14:26 +0200

    Comment on example

--- examples/mismatched_f64_elements.rs
+++ examples/mismatched_f64_elements.rs
@@ -2,6 +2,8 @@
 extern crate rulinalg;
 
 fn main() {
+    // The purpose of this example is to demonstrate the type of errors generated
+    // by `assert_matrix_eq!` when the comparison fails (which the below call will).
     let a = matrix![1.00, 2.00;
                     3.00, 4.00];
     let b = matrix![1.01, 2.00;
