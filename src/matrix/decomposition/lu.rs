d48c3445eb8f92f5261cbd9bbc362196862af340
James Lucas <AtheMathmo@users.noreply.github.com>
2016-11-04 09:45:34 +0000

    Fixing LU pivot bugs (#80)
    * Fixing LU pivot bugs
    
    * Adding regression tests for broken case

--- src/matrix/decomposition/lu.rs
+++ src/matrix/decomposition/lu.rs
@@ -1,13 +1,39 @@
-use matrix::{Matrix, BaseMatrix};
+use matrix::{Matrix, BaseMatrix, BaseMatrixMut};
 use error::{Error, ErrorKind};
 use utils;
 
 use std::any::Any;
 
-use libnum::{Float};
+use libnum::Float;
 
 impl<T> Matrix<T> where T: Any + Float
 {
+
+    fn produce_pivot(&self) -> Matrix<T> {
+        let n = self.rows();
+
+        let mut p = Matrix::<T>::identity(n);
+
+        // Compute the permutation matrix
+        for i in 0..n {
+            // Find the max value in each column
+            let mut curr_max_idx = i;
+            let mut curr_max = self[[i, i]];
+
+            for j in i+1..n {
+                if self[[j,i]] > curr_max {
+                    curr_max = self[[j,i]];
+                    curr_max_idx = j;
+                }
+            }
+
+            if curr_max_idx != i {
+                p.swap_rows(i, curr_max_idx);
+            }
+        }
+
+        p
+    }
 
     /// Computes L, U, and P for LUP decomposition.
     ///
@@ -38,52 +64,38 @@ impl<T> Matrix<T> where T: Any + Float
 
         let mut l = Matrix::<T>::zeros(n, n);
         let mut u = Matrix::<T>::zeros(n, n);
-
-        let mt = self.transpose();
-
-        let mut p = Matrix::<T>::identity(n);
-
-        // Compute the permutation matrix
-        for i in 0..n {
-            let (row,_) = utils::argmax(&mt.data[i*(n+1)..(i+1)*n]);
-
-            if row != 0 {
-                for j in 0..n {
-                    p.data.swap(i*n + j, row*n+j)
-                }
-            }
-        }
-
+        let p = self.produce_pivot();
         let a_2 = &p * self;
 
         for i in 0..n {
-            l.data[i*(n+1)] = T::one();
+            l[[i, i]] = T::one();
 
             for j in 0..i+1 {
                 let mut s1 = T::zero();
 
                 for k in 0..j {
-                    s1 = s1 + l.data[j*n + k] * u.data[k*n + i];
+                    s1 = s1 + l[[j, k]] * u[[k, i]];
                 }
 
-                u.data[j*n + i] = a_2[[j,i]] - s1;
+                u[[j ,i]] = a_2[[j,i]] - s1;
             }
 
             for j in i..n {
                 let mut s2 = T::zero();
 
                 for k in 0..i {
-                    s2 = s2 + l.data[j*n + k] * u.data[k*n + i];
+                    s2 = s2 + l[[j, k]] * u[[k, i]];
                 }
 
                 let denom = u[[i,i]];
 
-                if denom == T::zero() {
+                if denom.abs() < T::epsilon() {
                     return Err(Error::new(ErrorKind::DivByZero,
                         "Singular matrix found in LUP decomposition. \
                         A value in the diagonal of U == 0.0."));
                 }
-                l.data[j*n + i] = (a_2[[j,i]] - s2) / denom;
+                
+                l[[j, i]] = (a_2[[j,i]] - s2) / denom;
             }
 
         }
@@ -118,5 +130,25 @@ mod tests {
             Err(e) => assert!(*e.kind() == ErrorKind::DivByZero),
             Ok(_) => panic!()
         }
+    }
+
+    #[test]
+    fn test_basic_pivot() {
+        let a = matrix![5f64,4.,3.,2.,1.;
+                        4.,3.,2.,1.,5.;
+                        3.,2.,1.,5.,4.;
+                        2.,1.,5.,4.,3.;
+                        1.,5.,4.,3.,2.];
+        let p = a.produce_pivot();
+
+        let true_p = matrix![1f64,0.,0.,0.,0.;
+                            0.,0.,0.,0.,1.;
+                            0.,0.,0.,1.,0.;
+                            0.,0.,1.,0.,0.;
+                            0.,1.,0.,0.,0.];
+
+        assert!(p.data().iter()
+                    .zip(true_p.data().iter())
+                    .all(|(&x,&y)| (x-y).abs() == 0.0));
     }
 }
