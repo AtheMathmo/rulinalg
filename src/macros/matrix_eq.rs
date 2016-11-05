b76326c9397af6e949c1019cae397de23827791d
Andreas Longva <andreas.b.longva@gmail.com>
2016-11-05 22:30:09 +0100

    Initial docs for assert_matrix_eq!

--- src/macros/matrix_eq.rs
+++ src/macros/matrix_eq.rs
@@ -111,7 +111,7 @@ pub fn elementwise_matrix_comparison<T, M, C, E>(x: &M, y: &M, comparator: C) ->
             }
             mismatches
         };
-        
+
         if mismatches.is_empty() {
             MatrixComparisonResult::Match
         } else {
@@ -286,19 +286,140 @@ ULP tolerance: {ulp}",
 }
 
 
-/// Compare matrices for approximate equality.
+/// Compare matrices for exact or approximate equality.
+///
+/// The `assert_matrix_eq!` simplifies the comparison of two matrices by
+/// providing the following features:
+///
+/// - Verifies that the dimensions of the matrices match.
+/// - Offers both exact and approximate comparison of individual elements.
+/// - Multiple types of comparators available, depending on the needs of the user.
+/// - Built-in error reporting makes it easy to determine which elements of the two matrices
+///   that do not compare equal.
+///
+/// # Usage
+/// Given two matrices `x` and `y`, the default invocation performs an exact elementwise comparison of the two matrices.
+///
+/// ```
+/// # #[macro_use] extern crate rulinalg; fn main() { let x = matrix![1.0f64]; let y = matrix![1.0f64];
+/// // Performs elementwise exact comparison
+/// assert_matrix_eq!(x, y);
+/// # }
+/// ```
+///
+/// An exact comparison is often not desirable. In particular, with floating point types, rounding errors
+/// or other sources of inaccuracies tend to complicate the matter. For this purpose, `assert_matrix_eq!`
+/// provides several comparators.
+///
+/// ```
+/// # #[macro_use] extern crate rulinalg; fn main() {
+/// # let x = matrix![1.0f64]; let y = matrix![1.0f64];
+/// // Available comparators:
+/// assert_matrix_eq!(x, y, comp = exact);
+/// assert_matrix_eq!(x, y, comp = float);
+/// assert_matrix_eq!(x, y, comp = abs, tol = 1e-12);
+/// assert_matrix_eq!(x, y, comp = ulp, tol = 8);
+/// # }
+/// ```
+/// **Note**: The `comp` argument *must* be specified after `x` and `y`, and cannot come after comparator-specific options.
+/// This is a deliberate design decision, with the rationale that assertions should look as uniform as possible for
+/// the sake of readability.
+///
+///
+/// ### The `exact` comparator
+/// This comparator simply uses the default `==` operator to compare each pair of elements.
+/// The default comparator delegates the comparison to the `exact` comparator.
+///
+/// ### The `float` comparator
+/// The `float` comparator is designed to be a conservative default for comparing floating-point numbers.
+/// It is inspired by the `AlmostEqualUlpsAndAbs` comparison function proposed in the excellent blog post
+/// [Comparing Floating Point Numbers, 2012 Edition](https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/)
+/// by Bruce Dawson.
+///
+/// If you expect the two matrices to be almost exactly the same, but you want to leave some room for (very small) rounding errors,
+/// then this comparator should be your default choice.
+///
+/// The comparison criterion can be summarized as follows:
+///
+/// 1. If `assert_matrix_eq!(x, y, comp = abs, tol = max_eps)` holds for `max_eps` close to the machine epsilon for the floating point type,
+///    then the comparison is successful.
+/// 2. Otherwise, returns the result of `assert_matrix_eq!(x, y, comp = ulp, tol = max_ulp)`, where `max_ulp` is a small positive integer constant.
+///
+/// The `max_eps` and `max_ulp` parameters can be tweaked to your preference with the syntax:
+///
+/// ```
+/// # #[macro_use] extern crate rulinalg; fn main() {
+/// # let x = matrix![1.0f64]; let y = matrix![1.0f64];
+/// # let max_eps = 1.0; let max_ulp = 0;
+/// assert_matrix_eq!(x, y, comp = float, eps = max_eps, ulp = max_ulp);
+/// # }
+/// ```
+///
+/// These additional parameters can be specified in any order after the choice of comparator, and do not both need to be present.
+///
+/// ### The `abs` comparator
+/// Compares the absolute difference between individual elements against the specified tolerance. Specifically,
+/// for every pair of elements x and y picked from the same row and column in X and Y respectively,
+/// the criterion is defined by
+///
+/// ```text
+///     | x - y | <= tol.
+/// ```
+///
+/// In addition to floating point numbers, the comparator can also be used for integral numbers, both signed and unsigned.
+/// In order to avoid unsigned underflow, the difference is always computed by subtracting the smaller number from the larger number.
+/// Note that the type of `tol` is required to be the same as that of the scalar field.
+///
+///
+/// ### The `ulp` comparator
+/// Elementwise comparison of floating point numbers based on their [ULP](https://en.wikipedia.org/wiki/Unit_in_the_last_place) difference.
+/// Once again, this is inspired by the proposals
+/// [in the aforementioned blog post by Bruce Dawon]
+/// (https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/),
+/// but it handles some cases explicitly as to provide better error reporting.
+///
+/// Note that the ULP difference of two floating point numbers is not defined in the following cases:
+///
+/// - The two numbers have different signs. The only exception here is +0 and -0, which are considered an exact match.
+/// - One of the numbers is NaN.
+///
+/// ULP-based comparison is typically used when two numbers are expected to be very, very close to each other. However,
+/// it is typically not very useful very close to zero, which is discussed in the linked blog post above.
+/// The error in many mathematical functions can often be bounded by a certain number of ULP, and so
+/// this comparator is particularly useful if this number is known.
+///
+/// Note that the scalar type of the matrix must implement the [Ulp trait](ulp/trait.Ulp.html) in order
+/// to be used with this comparator. By default, `f32` and `f64` implementations are provided.
+///
+/// # Error reporting
+///
+/// TODO: An example of how the macro reports comparison failures. (do this as one of the last steps, after the format has been finalized)
+///
 /// # Examples
 ///
 /// ```
 /// #[macro_use]
 /// extern crate rulinalg;
+/// use rulinalg::matrix::Matrix;
 ///
 /// # fn main() {
-/// let a = matrix![1.000, 2.000,
-///                 3.000, 4.000];
-/// let b = matrix![0.999, 2.001,
-///                 2.998, 4.000 ];
-/// assert_matrix_eq!(a, b, comp = abs, tol = 0.01);
+/// let ref a = matrix![1, 2;
+///                 3, 4i64];
+/// let ref b = matrix![1, 3;
+///                 3, 4i64];
+///
+/// let ref x = matrix![1.000, 2.000,
+///                 3.000, 4.000f64];
+/// let ref y = matrix![0.999, 2.001,
+///                 2.998, 4.000f64];
+///
+///
+/// // comp = abs is also applicable to integers
+/// assert_matrix_eq!(a, b, comp = abs, tol = 1);
+/// assert_matrix_eq!(x, y, comp = abs, tol = 0.01);
+///
+/// assert_matrix_eq!(a * 2, a + a);
+/// assert_matrix_eq!(x * 2.0, x + x, comp = float);
 /// # }
 /// ```
 #[macro_export]
