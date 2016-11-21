c3b477ac3e5d99206168093529083266d1753574
Andreas Longva <andreas.b.longva@gmail.com>
2016-11-21 17:54:02 +0100

    First draft of LU redesign

--- src/matrix/decomposition/lu.rs
+++ src/matrix/decomposition/lu.rs
@@ -1,15 +1,44 @@
-use matrix::{Matrix, BaseMatrixMut};
+use matrix::{Matrix, BaseMatrixMut, forward_substitution, back_substitution};
+use vector::Vector;
 use error::{Error};
+use matrix::Decomposition;
 
 use std::any::Any;
 
 use libnum::Float;
 
-impl<T> Matrix<T> where T: Any + Float
-{
+/// The L, U and P matrices in the LUP decomposition.
+#[derive(Debug, Clone)]
+pub struct LU<T> {
+    /// The lower triangular matrix in the LUP decomposition.
+    pub l: Matrix<T>,
+    /// The upper triangular matrix in the LUP decomposition.
+    pub u: Matrix<T>,
+    /// The permutation matrix in the LUP decomposition.
+    pub p: Matrix<T>
+}
+
+/// TODO
+#[derive(Debug, Clone)]
+pub struct LuDecomposition<T> {
+    // For now, we store the separate factors, but in the future
+    // we can greatly reduce memory usage by storing both L and U
+    // in the space of a single matrix
+    lu: LU<T>
+}
+
+impl<T> Decomposition for LuDecomposition<T> {
+    type Factors = LU<T>;
+
+    fn decompose(self) -> Self::Factors {
+        self.lu
+    }
+}
+
+impl<T> Matrix<T> where T: Any + Float {
     /// Computes the LUP decomposition.
     ///
-    /// For a given square matrix `A`, returns `(L, U, P)` such that
+    /// For a given square matrix `A`, returns L, U, P such that
     ///
     /// ```text
     /// PA = LU
@@ -21,27 +50,23 @@ impl<T> Matrix<T> where T: Any + Float
     /// # Examples
     ///
     /// ```
-    /// use rulinalg::matrix::Matrix;
+    /// use rulinalg::matrix::{Matrix, Decomposition, LU};
     ///
     /// let a = Matrix::new(3,3, vec![1.0, 2.0, 0.0,
     ///                               0.0, 3.0, 4.0,
     ///                               5.0, 1.0, 2.0]);
     ///
-    /// let (l,u,p) = a.lup_decomp().expect("This matrix should decompose!");
+    /// let LU { l, u, p } = a.lu().decompose();
     /// ```
     ///
     /// # Panics
     ///
     /// - Matrix is not square.
-    ///
-    /// # Failures
-    ///
-    /// - Can not fail. `Result` will be removed in future releases.
-    pub fn lup_decomp(&self) -> Result<(Matrix<T>, Matrix<T>, Matrix<T>), Error> {
+    pub fn lu(self) -> LuDecomposition<T> {
         let n = self.cols;
         assert!(self.rows == n, "Matrix must be square for LUP decomposition.");
         let mut l = Matrix::<T>::zeros(n, n);
-        let mut u = self.clone();
+        let mut u = self;
         let mut p = Matrix::<T>::identity(n);
 
         for index in 0..n {
@@ -78,20 +103,37 @@ impl<T> Matrix<T> where T: Any + Float
                 }
             }
         }
-        Ok((l, u, p))
+
+        LuDecomposition {
+            lu: LU {
+                l: l,
+                u: u,
+                p: p
+            }
+        }
+    }
+}
+
+
+impl<T> LuDecomposition<T> where T: Any + Float {
+    /// TODO
+    pub fn solve(&self, b: Vector<T>) -> Result<Vector<T>, Error> {
+        let b = try!(forward_substitution(&self.lu.l, &self.lu.p * b));
+        back_substitution(&self.lu.u, b)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use matrix::Matrix;
+    use matrix::{Matrix, Decomposition};
+    use matrix::LU;
 
     #[test]
     #[should_panic]
     fn test_non_square_lup_decomp() {
         let a: Matrix<f64> = Matrix::ones(2, 3);
 
-        let _ = a.lup_decomp();
+        let _ = a.lu();
     }
 
     #[test]
@@ -111,7 +153,7 @@ mod tests {
             let x = matrix![2.0, 5.0, 3.0;
                             0.0, 0.0, 1.0;
                             0.0, 0.0, 3.0];
-            let (l, u, p) = x.lup_decomp().unwrap();
+            let LU { l, u, p } = x.clone().lu().decompose();
             assert_matrix_eq!(x, p.transpose() * l * u, comp = float);
         }
 
@@ -119,7 +161,7 @@ mod tests {
             let x = matrix![2.0, 0.0, 0.0;
                             5.0, 0.0, 0.0;
                             3.0, 1.0, 3.0];
-            let (l, u, p) = x.lup_decomp().unwrap();
+            let LU { l, u, p } = x.clone().lu().decompose();
             assert_matrix_eq!(x, p.transpose() * l * u, comp = float);
         }
 
@@ -127,7 +169,7 @@ mod tests {
             let x = matrix![1.0, 2.0, 3.0;
                             3.0, 2.0, 1.0;
                             4.0, 4.0, 4.0];
-            let (l, u, p) = x.lup_decomp().unwrap();
+            let LU { l, u, p } = x.clone().lu().decompose();
             assert_matrix_eq!(x, p.transpose() * l * u, comp = float);
         }
     }
