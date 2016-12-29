fcc563c0ae1010ea874804c5fdf4fcde22471000
James Lucas <LucasJ94@hotmail.co.uk>
2016-12-29 19:12:46 +0000

    Updating API documentation to v0.4.1

--- doc/implementors/core/ops/trait.DerefMut.js
+++ doc/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rulinalg"] = ["impl&lt;'a,&nbsp;T:&nbsp;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rulinalg/matrix/struct.RowMut.html' title='rulinalg::matrix::RowMut'>RowMut</a>&lt;'a,&nbsp;T&gt;","impl&lt;'a,&nbsp;T:&nbsp;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rulinalg/matrix/struct.ColumnMut.html' title='rulinalg::matrix::ColumnMut'>ColumnMut</a>&lt;'a,&nbsp;T&gt;",];
+implementors["rulinalg"] = ["impl&lt;'a, T:&nbsp;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rulinalg/matrix/struct.RowMut.html' title='rulinalg::matrix::RowMut'>RowMut</a>&lt;'a, T&gt;","impl&lt;'a, T:&nbsp;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='rulinalg/matrix/struct.ColumnMut.html' title='rulinalg::matrix::ColumnMut'>ColumnMut</a>&lt;'a, T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/implementors/core/ops/trait.Not.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rulinalg"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a>&lt;Output=T&gt; + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='rulinalg/vector/struct.Vector.html' title='rulinalg::vector::Vector'>Vector</a>&lt;T&gt;","impl&lt;'a, T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a>&lt;Output=T&gt; + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for &amp;'a <a class='struct' href='rulinalg/vector/struct.Vector.html' title='rulinalg::vector::Vector'>Vector</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
