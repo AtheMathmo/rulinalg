acfa8d3c7b2f73f90e7dcd40e250172551280e94
Andreas Longva <andreas.b.longva@gmail.com>
2017-01-07 11:16:52 +0100

    Preliminary documentation for PermutationMatrix

--- src/matrix/permutation_matrix.rs
+++ src/matrix/permutation_matrix.rs
@@ -6,7 +6,84 @@ use utils::Permutation;
 
 use libnum::Num;
 
-/// TODO
+/// An efficient implementation of a permutation matrix.
+///
+/// A [permutation matrix](https://en.wikipedia.org/wiki/Permutation_matrix)
+/// is a very special kind of matrix. It is essentially a matrix representation
+/// of the more general concept of a permutation. That is, an `n` x `n` permutation
+/// matrix corresponds to a permutation of ordered sets whose cardinality is `n`.
+/// In particular, given an `m` x `n` matrix `A` and an `n` x `n` permutation
+/// matrix `P`, the action of left-multiplying `A` by `P`, `PA`, corresponds
+/// to permuting the rows of `A` by the given permutation represented by `P`.
+/// Conversely, right-multiplication corresponds to column permutation.
+/// More precisely, given another permutation matrix `K` of size `m` x `m`,
+/// then `AK` is the corresponding permutation of the columns of `A`.
+///
+/// Due to their unique structure, permutation matrices can be much more
+/// efficiently represented and applied than general matrices. Recall that
+/// for general matrices `X` and `Y` of size `m` x `m` and `n` x `n` respectively,
+/// the storage of `X` requires O(`m`<sup>2</sup>) memory and the storage of
+/// `Y` requires O(`n`<sup>2</sup>) memory. Ignoring for the moment the existence
+/// of Strassen's matrix multiplication algorithm and more theoretical alternatives,
+/// the multiplication `XA` requires O(`m`<sup>2</sup>`n`) operations, and
+/// the multiplication `AY` requires O(`m``n`<sup>2</sup>) operations.
+///
+/// By constrast, the storage of `P` requires only O(`m`) memory, and
+/// the storage of `K` requires O(`n`) memory. Moreover, the products
+/// `PA` and `AK` both require merely O(`mn`) operations.
+///
+/// # Representation
+/// A permutation of an ordered set of cardinality *n* is a map of the form
+///
+/// ```text
+/// p: { 1, ..., n } -> { 1, ..., n }.
+/// ```
+///
+/// That is, for any index `i`, the permutation `p` sends `i` to some
+/// index `j = p(i)`, and hence the map may be represented as an array of integers
+/// of length *n*.
+///
+/// # Examples
+/// ```
+/// # #[macro_use] extern crate rulinalg; fn main() {
+/// use rulinalg::matrix::PermutationMatrix;
+///
+/// let ref x = matrix![1, 2, 3;
+///                     4, 5, 6;
+///                     7, 8, 9];
+///
+/// // Swap the two first rows of x by left-multiplying a permutation matrix
+/// let expected = matrix![4, 5, 6;
+///                        1, 2, 3;
+///                        7, 8, 9];
+/// let mut p = PermutationMatrix::identity(3);
+/// p.swap(0, 1);
+/// assert_eq!(expected, p * x);
+///
+/// // Swap the two last columns of x by right-multiplying a permutation matrix
+/// let expected = matrix![1, 3, 2;
+///                        4, 6, 5;
+///                        7, 9, 8];
+/// let mut p = PermutationMatrix::identity(3);
+/// p.swap(1, 2);
+/// assert_eq!(expected, x * p);
+///
+/// // One can also construct the same permutation matrix directly
+/// // from an array representation.
+/// let ref p = PermutationMatrix::from_array(vec![0, 2, 1]).unwrap();
+/// assert_eq!(expected, x * p);
+///
+/// // One may also obtain a full matrix representation of the permutation
+/// assert_eq!(p.as_matrix(), matrix![1, 0, 0;
+///                                   0, 0, 1;
+///                                   0, 1, 0]);
+///
+/// // The inverse of a permutation matrix can efficiently be obtained
+/// let p_inv = p.inverse();
+/// // TODO: Implement product of permutation matrices to show that
+/// // the p * p_inv is the identity
+/// # }
+/// ```
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct PermutationMatrix<T> {
     // A permutation matrix of dimensions NxN is represented as a permutation of the rows
