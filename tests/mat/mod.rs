ec25eb8c695400f0232f77225534f3517b225796
Andreas Longva <andreas.b.longva@gmail.com>
2017-02-09 22:42:14 +0100

    Port existing lup_decomp test to PartialPivLu

--- tests/mat/mod.rs
+++ tests/mat/mod.rs
@@ -90,6 +90,89 @@ fn matrix_lup_decomp() {
 }
 
 #[test]
+fn matrix_partial_piv_lu() {
+    use rulinalg::matrix::decomposition::{LUP, PartialPivLu};
+    use rulinalg::matrix::decomposition::Decomposition;
+    // This is a port of the test for the old lup_decomp
+    // function, using the new PartialPivLu struct.
+
+    {
+        // Note: this test only works with a _specific_
+        // implementation of LU decomposition, because
+        // an LUP decomposition is not in general unique,
+        // so one cannot test against expected L, U and P
+        // matrices unless one knows exactly which ones the
+        // algorithm works.
+        let a = matrix![1., 3., 5.;
+                        2., 4., 7.;
+                        1., 1., 0.];
+
+        let LUP { l, u, p } = PartialPivLu::decompose(a)
+                                        .expect("Matrix is well-conditioned")
+                                        .unpack();
+
+        let l_true = matrix![1.0,  0.0,  0.0;
+                             0.5,  1.0,  0.0;
+                             0.5, -1.0,  1.0];
+        let u_true = matrix![2.0,  4.0,  7.0;
+                             0.0,  1.0,  1.5;
+                             0.0,  0.0, -2.0];
+        let p_true = matrix![0.0, 1.0, 0.0;
+                            1.0, 0.0, 0.0;
+                            0.0, 0.0, 1.0];
+
+        assert_matrix_eq!(l, l_true, comp = float);
+        assert_matrix_eq!(u, u_true, comp = float);
+        assert_matrix_eq!(p.as_matrix(), p_true, comp = float);
+    }
+
+    {
+        let b = matrix![1., 2., 3., 4., 5.;
+                        3., 0., 4., 5., 6.;
+                        2., 1., 2., 3., 4.;
+                        0., 0., 0., 6., 5.;
+                        0., 0., 0., 5., 6.];
+
+        let LUP { l, u, p } = PartialPivLu::decompose(b.clone())
+                         .expect("Matrix is well-conditioned.")
+                         .unpack();
+        let k = p.inverse() * l * u;
+
+        assert_matrix_eq!(k, b, comp = float);
+    }
+
+    {
+        let c = matrix![-4.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+                        1.0, -4.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0;
+                        0.0, 1.0, -4.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0;
+                        1.0, 0.0, 0.0, -4.0, 1.0, 0.0, 1.0, 0.0, 0.0;
+                        0.0, 1.0, 0.0, 1.0, -4.0, 1.0, 0.0, 1.0, 0.0;
+                        0.0, 0.0, 1.0, 0.0, 1.0, -4.0, 0.0, 0.0, 1.0;
+                        0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -4.0, 1.0, 0.0;
+                        0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, -4.0, 1.0;
+                        0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, -4.0];
+
+        let LUP { l, u, p } = PartialPivLu::decompose(c.clone())
+                                .unwrap()
+                                .unpack();
+        let c_reconstructed = p.inverse() * l * u;
+        assert_matrix_eq!(c_reconstructed, c, comp = float);
+    }
+
+    {
+        let d = matrix![1.0, 1.0, 0.0, 0.0;
+                        0.0, 0.0, 1.0, 0.0;
+                        -1.0, 0.0, 0.0, 0.0;
+                        0.0, 0.0, 0.0, 1.0];
+        let LUP { l, u, p } = PartialPivLu::decompose(d.clone())
+                                .unwrap()
+                                .unpack();
+        let d_reconstructed = p.inverse() * l * u;
+        assert_matrix_eq!(d_reconstructed, d, comp = float);
+    }
+}
+
+#[test]
 fn cholesky() {
     let a = matrix![25., 15., -5.;
                     15., 18., 0.;
