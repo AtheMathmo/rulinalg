8b398a1df20a9c6510acf15f8fa3401793acf125
Andreas Longva <andreas.b.longva@gmail.com>
2017-02-14 19:07:02 +0100

    Add benchmarks for Cholesky::solve

--- benches/linalg/cholesky.rs
+++ benches/linalg/cholesky.rs
@@ -47,3 +47,23 @@ fn cholesky_500x500(b: &mut Bencher) {
         x.cholesky().expect("Matrix is invertible")
     })
 }
+
+#[bench]
+fn cholesky_solve_1000x1000(b: &mut Bencher) {
+    let n = 1000;
+    let x = Matrix::identity(n);
+    let cholesky = Cholesky::decompose(x).unwrap();
+    b.iter(|| {
+        cholesky.solve(vector![0.0; n])
+    });
+}
+
+#[bench]
+fn cholesky_solve_100x100(b: &mut Bencher) {
+    let n = 100;
+    let x = Matrix::identity(n);
+    let cholesky = Cholesky::decompose(x).unwrap();
+    b.iter(|| {
+        cholesky.solve(vector![0.0; n])
+    });
+}
