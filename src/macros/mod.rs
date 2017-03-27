17fc4b1da1b844e2a296b7834f483f64c8657d5c
Andreas Longva <andreas.b.longva@gmail.com>
2017-03-27 09:50:45 +0200

    Implement assert_scalar_eq!

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -12,6 +12,9 @@ mod assert_matrix_eq;
 #[macro_use]
 mod assert_vector_eq;
 
+#[macro_use]
+mod assert_scalar_eq;
+
 mod comparison;
 
 pub use self::comparison::{
@@ -27,3 +30,4 @@ pub use self::comparison::{
 
 pub use self::assert_matrix_eq::elementwise_matrix_comparison;
 pub use self::assert_vector_eq::elementwise_vector_comparison;
+pub use self::assert_scalar_eq::scalar_comparison;
