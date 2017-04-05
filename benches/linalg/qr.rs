24c9703af6ba6effba8eaa6a867252a0c0e6d2aa
Andreas Longva <andreas.b.longva@gmail.com>
2017-04-05 20:37:37 +0200

    Benchmarks for Thin QR

--- benches/linalg/qr.rs
+++ benches/linalg/qr.rs
@@ -53,6 +53,30 @@ fn householder_qr_decompose_unpack_100x500(b: &mut Bencher) {
 }
 
 #[bench]
+fn householder_qr_decompose_unpack_thin_100x100(b: &mut Bencher) {
+    let x = reproducible_random_matrix(100, 100);
+    b.iter(|| {
+        HouseholderQr::decompose(x.clone()).unpack_thin()
+    })
+}
+
+#[bench]
+fn householder_qr_decompose_unpack_thin_500x100(b: &mut Bencher) {
+    let x = reproducible_random_matrix(500, 100);
+    b.iter(|| {
+        HouseholderQr::decompose(x.clone()).unpack_thin()
+    })
+}
+
+#[bench]
+fn householder_qr_decompose_unpack_thin_100x500(b: &mut Bencher) {
+    let x = reproducible_random_matrix(100, 500);
+    b.iter(|| {
+        HouseholderQr::decompose(x.clone()).unpack_thin()
+    })
+}
+
+#[bench]
 #[allow(deprecated)]
 fn qr_decomp_100x100(b: &mut Bencher) {
     let x = reproducible_random_matrix(100, 100);
@@ -65,14 +89,6 @@ fn qr_decomp_100x100(b: &mut Bencher) {
 #[allow(deprecated)]
 fn qr_decomp_100x500(b: &mut Bencher) {
     let x = reproducible_random_matrix(100, 500);
-    b.iter(|| {
-        x.clone().qr_decomp()
-    })
-}
-
-#[bench]
-fn qr_decomp_500x100(b: &mut Bencher) {
-    let x = reproducible_random_matrix(500, 100);
     b.iter(|| {
         x.clone().qr_decomp()
     })
